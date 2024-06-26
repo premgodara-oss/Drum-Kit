8c8b3a76baab79e9cac4114dbb524764b7baa543
Parma ram Godara <123255640+premgodara-oss@users.noreply.github.com>
2024-06-26 14:51:15 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,42 @@
+var btnlen=document.querySelectorAll(".drum").length;
+for(var i=0; i<btnlen;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+      //indentity button using this and getting this inner html
+        var buttoninnerhtml=this.innerHTML;
+        switch(buttoninnerhtml){
+            case "w":
+                // alert("running");
+                var audf=new Audio("sounds/tom-1.mp3");
+                audf.play();
+                this.style.color="red";
+                break; 
+            case "a":
+                var auda=new Audio("sounds/tom-2.mp3");
+                auda.play();
+                break;
+            case "s":
+                var auds=new Audio("sounds/tom-3.mp3");
+                auds.play();
+                break; 
+            case "d":
+                var audd=new Audio("sounds/tom-4.mp3");
+                audd.play();
+                break;
+            case "j":
+                var audj=new Audio("sounds/snare.mp3");
+                audj.play();
+                break;
+            case "k":
+                var audk=new Audio("sounds/kick-bass.mp3");
+                audk.play();
+                break; 
+            case "i":
+                var audi=new Audio("sounds/crash.mp3");
+                audi.play();
+                break; 
+            default:console.log (buttoninnerhtml);  
+    }
+ } ) ;
+    }
+
+
